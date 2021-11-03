fde1fbb8cd983bb3a4470417df30079ef77c73a4
Aryanlivi <aryanlivi@gmail.com>
2021-11-03 22:55:57 +0545

    Initial commit

---
+++ box2.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(rotation_degrees);
+	 # Replace with function body.
+
+func _physics_process(delta):
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
