cedf4449def8da16366e12b9f578f78cf2b84874
Aryanlivi <aryanlivi@gmail.com>
2021-11-12 23:46:27 +0545

    stickfigure animation

--- ManSprite.gd
+++ ManSprite.gd
@@ -3,8 +3,9 @@ extends AnimatedSprite
 onready var pathfollow=get_parent().get_parent().get_parent()
 
 func _process(delta):
-	if(pathfollow.unit_offset==1):
-		self.frame=0
+	if(pathfollow.unit_offset==1 or Game.gamestate!=Game.state.PULL):
+		self.animation="idle"
 		self.playing=false
 	if(Game.gamestate==Game.state.PULL):
+		self.animation="pull"
 		self.playing=true
